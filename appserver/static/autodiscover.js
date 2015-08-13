f6b18b991fdc2dbdd274883704d091c683609caa
Richard Morgan <silkyrich@gmail.com>
2015-08-13 19:12:53 +0100

    not really sure what has changed !

--- appserver/static/autodiscover.js
+++ appserver/static/autodiscover.js
@@ -3,6 +3,7 @@ require.config({
         "app": "../app"
     }
 });
+
 require(['splunkjs/mvc/simplexml/ready!'], function(){
     require(['splunkjs/ready!'], function(){
         // The splunkjs/ready loader script will automatically instantiate all elements
